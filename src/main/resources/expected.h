2ab73c6592579f8209de2f67f4cd528543b90bc3
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-01-29 14:26:27 +0100

    Fix example header

--- src/main/resources/expected.h
+++ src/main/resources/expected.h
@@ -5,10 +5,6 @@
 @property (readonly) Wheel wheel1
 @property (readonly) Wheel wheel2
 @property (readonly) Wheel wheel3
-@property (readonly) Wheel wheel4
-@end
-
-@interface RaceCar: BasicCar
 @end
 
 @interface DeliveryTruck: BasicCar
